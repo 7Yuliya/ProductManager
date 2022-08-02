513ad7216a2c56bf3be7f88b2cabb65ef3b592a2
Юлия Алексина <uliyaalexina@mail.ru>
2022-08-02 13:39:45 -0700

    Fixed src/

--- src/test/java/org/example/ProductManagerTest.java
+++ src/test/java/org/example/ProductManagerTest.java
@@ -14,36 +14,62 @@ public class ProductManagerTest {
     Product product2 = new Book(2, "Князь Серебряный", 1_200, "Алексей Константинович Толстой");
     Product product3 = new Smartphone(3, "Самсунг", 15_000, "Южнокорейская группа компаний");
     Product product4 = new Smartphone(4, "Apple", 35_000, "Аиериканская корпорация");
-
+    Product product5 = new Smartphone(5, "Самсунг", 25_000, "Южнокорейская группа компаний");
 
     @BeforeEach
+
     public void setup() {
 
         manager.add(product1);
         manager.add(product2);
         manager.add(product3);
         manager.add(product4);
+        manager.add(product5);
     }
 
     @Test
     public void ShouldAddProduct() {
 
-
         Product[] actual = manager.findAll();
-        Product[] expected = {product1, product2, product3, product4};
+        Product[] expected = {product1, product2, product3, product4, product5};
 
         assertArrayEquals(expected, actual);
     }
 
     @Test
 
-    public void ShouldSearchByProduct() {
+
+    public void ShouldSearchByProduct2() {
 
 
         Product[] actual = manager.searchBy("Самсунг");
-        Product[] expected = {product3};
+        Product[] expected = {product5, product3};
 
         assertArrayEquals(expected, actual);
     }
 
+    @Test
+
+
+    public void ShouldSearchByProduct1() {
+
+
+        Product[] actual = manager.searchBy("Apple");
+        Product[] expected = {product4};
+
+        assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+
+
+    public void ShouldSearchByProduct0() {
+
+
+        Product[] actual = manager.searchBy("Марта");
+        Product[] expected = {};
+
+        assertArrayEquals(expected, actual);
+    }
 }
--- src/test/java/org/example/ProductRepositoryTest.java
+++ src/test/java/org/example/ProductRepositoryTest.java
@@ -10,7 +10,7 @@ public class ProductRepositoryTest {
     Product product2 = new Book(2, "Князь Серебряный", 1_200, "Алексей Константинович Толстой");
     Product product3 = new Smartphone(3, "Самсунг", 15_000, "Южнокорейская группа компаний");
     Product product4 = new Smartphone(4, "Apple", 35_000, "Аиериканская корпорация");
-
+    Product product5 = new Smartphone(5, "Самсунг", 25_000, "Южнокорейская группа компаний");
 
     @Test
     public void test() {
@@ -19,10 +19,11 @@ public class ProductRepositoryTest {
         repo.save(product2);
         repo.save(product3);
         repo.save(product4);
+        repo.save(product5);
         repo.removeById(product2.getId());
 
 
-        Product[] expected = {product4, product3, product1};
+        Product[] expected = {product5, product4, product3, product1};
         Product[] actual = repo.findAll();
 
         assertArrayEquals(expected, actual);
